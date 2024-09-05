1190586395895cde43663924f2346bfe952eb0e4
kanisimoff <k.anisimov@part-place.ru>
2024-09-06 02:36:25 +0700

    add the README and test data

---
+++ data.sql
@@ -0,0 +1,26 @@
+insert into players
+values('1', '1');
+
+insert into players
+values('2', '2');
+
+insert into players
+values('3', '3');
+
+insert into levels
+values('1', 'first', '1');
+
+insert into levels
+values('2', 'second', '2');
+
+insert into prizes
+values('1', 'gold');
+
+insert into prizes
+values('2', 'diamond');
+
+insert into player_levels
+values('1', '2', '1', '2024.09.04', true, '1000');
+
+insert into player_levels
+values('2', '2', '2', '2024.09.05', false, '0');
